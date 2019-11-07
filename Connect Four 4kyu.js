5a2e7273878b64c0689d3daf86ebba0ded4dd00e
Nanotorre <fernandodelatorre4@gmail.com>
2019-11-07 22:09:02 +0100

    connect4

--- Connect Four 4kyu.js
+++ Connect Four 4kyu.js
@@ -7,8 +7,9 @@ function whoIsWinner(piecesPositionList){
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0]
   ]
-  piecesPositionList.forEach(piece => {
-     let pos = [5, piece.charCodeAt(0)-65 ]
+
+   for(let piece of piecesPositionList) {
+     let pos = [5, piece.charCodeAt(0)-65]
      while(grid[pos[0]][pos[1]]!==0) {
        pos[0]-=1
      }
@@ -17,15 +18,40 @@ function whoIsWinner(piecesPositionList){
       grid[pos[0]][pos[1]]="R"
     }
     else {
-       grid[pos[0]][pos[1]]="L"
-       
-
+       grid[pos[0]][pos[1]]="Y"
     }
-        console.log()
-    
-       
-  })
+
+    for (let i=0; i<7; i++) {
+
+          if(grid[5][i]=== 'Y'&& grid[4][i]==='Y' &&  grid[3][i]==='Y' && grid[2][i]==='Y' ||
+             grid[4][i]=== 'Y'&& grid[3][i]==='Y' &&  grid[2][i]==='Y' && grid[1][i]==='Y' ||
+             grid[3][i]=== 'Y'&& grid[2][i]==='Y' &&  grid[1][i]==='Y' && grid[0][i]==='Y') 
+          
+            return "yellow"
+          if (grid[5][i]=== 'R'&& grid[4][i]==='R' &&  grid[3][i]==='R' && grid[2][i]==='R' ||
+             grid[4][i]=== 'R'&& grid[3][i]==='R' &&  grid[2][i]==='R' && grid[1][i]==='R' ||
+             grid[3][i]=== 'R'&& grid[2][i]==='R' &&  grid[1][i]==='R' && grid[0][i]==='R') {
+
+              return "red"
+             }
+        } 
+
+    for (let arr of grid) {
+         let "Y", "R";
+        if(arr.forEach(piece=> {
+          if(piece==="Y" && piece
+          
+        }))
+    }
+
+
+        if(grid.every(piece=> piece.every(pos=>pos!==0))) {
+          return "draw"
+        }
+   }   
+
   console.log(grid)
+ 
 }
 
 
@@ -38,4 +64,4 @@ function whoIsWinner(piecesPositionList){
                         "G_Red",
                         "B_Yellow"]
                        
-whoIsWinner(piecesPositionList)
+console.log(whoIsWinner(piecesPositionList))
